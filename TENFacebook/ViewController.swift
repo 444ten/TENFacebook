c234925d5e108f1ba9aa6074b79e86e349ffa279
444ten <444ten@gmail.com>
2015-11-16 15:42:03 +0200

    initial project

---
+++ TENFacebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TENFacebook
+//
+//  Created by 444ten on 11/16/15.
+//  Copyright © 2015 444ten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
